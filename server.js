cfb404c5f1dc051e8f625eb42d17202ecea977ac
Kautilya Tripathi <tripathi.kautilya@gmail.com>
2017-04-16 15:57:41 +0000

    initial commit

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var months = ['January','February','March','April','May','June','July','August',
 var year = a.getFullYear();
 var month = months[a.getMonth()];
 var date = a.getDate();
-var time = month + ' ' + date + ', ' + year;
+var time = date + ' ' + month + ', ' + year;
 return time;
 }
 
